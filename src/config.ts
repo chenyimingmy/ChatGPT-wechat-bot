0b83142e986957d804b6c8eb511782985f0a4fa2
chenyimingmy <98199843+chenyimingmy@users.noreply.github.com>
2023-02-09 20:06:19 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -4,9 +4,9 @@ export default {
   // 设置获取消息的重试次数
   retryTimes: 3,
   // 在群组中设置唤醒微信机器人的关键词
-  groupKey: '',
+  groupKey: '小天',
   // 在私聊中设置唤醒微信机器人的关键词
-  privateKey: '',
+  privateKey: '小天',
   // 重置上下文的关键词，如可设置为reset
   resetKey: 'reset',
   // 开启会后收到ChatGPT的自动回复
